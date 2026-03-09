2dfb9543e7242bbfe9071b0746f067054d9513ae
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-03-09 13:43:30 +0300

    applied docstrings

--- models.py
+++ models.py
@@ -2,31 +2,36 @@ import numpy as np
 from numpy.typing import NDArray
 
 class SimpleLinearRegression:
+    """Simple linear regression model with one input and one output.
     """
-    - simple linear regression
-    - accepts the training dataset
-    - do the calculations Ex, Ey, Exy, Ex^2
-    - solve for the parameters
-    """
-    def __init__(self, input_train_data: NDArray[np.float64], output_train_data: NDArray[np.float64]) -> None: 
-        """
-        Input Type (of np.array): [x1,x2,...,xn], [y1,y2,...,yn]
-        """
+    def _nit__(self, input_train_data: NDArray[np.float64], output_train_data: NDArray[np.float64]) -> None: 
         self.input_train_data: NDArray[np.float64] = input_train_data
         self.output_train_data: NDArray[np.float64] = output_train_data
         self.n: int = len(input_train_data)
 
         self._calc_()
-        self._solve_()
+        self._fit_()
     
     def _calc_(self) -> None:
+        """Calculates key terms later used in the calculations of finding the parameters.
+        """
         self.Ex = np.sum(self.input_train_data)
         self.Ey = np.sum(self.output_train_data)
         self.Exy = np.sum(self.input_train_data * self.output_train_data)
         self.Ex_sqr = np.sum(pow(self.input_train_data, 2))
     
-    def _solve_(self) -> None:
-        common_denominator = (self.n * self.Ex_sqr) - (self.Ex * self.Ex)
+    def _fit_(self) -> None:
+        r"""Solves for the best fit parameters b0 and b1. 
+        
+        Below are the two formulas (in LaTeX) used to solve for the parameters:
+
+        $$b_0 = \frac{ \Sigma{y}\Sigma{x^2} - \Sigma{x}\Sigma{xy} }
+            { n\Sigma{x^2} - (\Sigma{x})^2 }$$
+
+        $$b_1 = \frac{ n\Sigma{xy} - \Sigma{x}\Sigma{y} }
+            { n\Sigma{x^2} - (\Sigma{x})^2 }$$
+        """
+        common_denominator = (self.n * self.Ex_sqr) - pow(self.Ex, 2)
         self.b0 = ( (self.Ey * self.Ex_sqr) - (self.Ex * self.Exy) ) / common_denominator
         self.b1 = ( (self.n * self.Exy) - (self.Ex * self.Ey) ) / common_denominator
 
@@ -34,22 +39,37 @@ class SimpleLinearRegression:
         return self.b0, self.b1
 
     def predict(self, input_test_data: NDArray[np.float64]) -> NDArray[np.float64]:
-        """
-        Input Type (of np.array): [x1,x2,...,xn]
-        Output Type (of np.array): [y1,y2,...,yn]
-        """
         return self.b1 * input_test_data + self.b0
     
 class ModelKit:
     def MAE(self, dataset_1: NDArray[np.float64], dataset_2: NDArray[np.float64]) -> np.float64:
-        """
-        Returns the mean absolute error between the two given datasets.
+        """Returns the mean absolute error between two datasets.
+
+        Parameters
+        ----------
+        dataset_1 : NDArray[np.float64]
+        dataset_2 : NDArray[np.float64]
+
+        Returns
+        -------
+        np.float64
         """
         return np.sum(abs(dataset_1 - dataset_2)) / len(dataset_2)
     
     def split_data(self, dataset: NDArray[np.float64], train_percentage: float) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
-        """
-        Returns (train_data, test_data)
+        """Splits a dataset into train and test data categories using the provided percentage value.
+
+        Parameters
+        ----------
+        dataset : NDArray[np.float64]
+        train_percentage : float
+            The size of the train data.
+
+        Returns
+        -------
+        tuple[NDArray[np.float64], NDArray[np.float64]]
+            [0] is the train data
+            [1] is the test data
         """
         split_size = int(len(dataset) * train_percentage)
         train_data = dataset[:split_size]
@@ -61,8 +81,17 @@ class StatKit:
         return np.sum(dataset) / len(dataset)
     
     def get_var_std(self, dataset: NDArray[np.float64]) -> tuple[np.float64, np.float64]:
-        """
-        Returns (variance, standard deviation)
+        """Returns the variance and standard deviation of the given dataset.
+
+        Parameters
+        ----------
+        dataset : NDArray[np.float64]
+
+        Returns
+        -------
+        tuple[np.float64, np.float64]
+            [0] is variance
+            [1] is standard deviation
         """
         n = len(dataset)
         mean = self.get_mean(dataset)
@@ -70,10 +99,17 @@ class StatKit:
         variance = (sqr_sum / n) - pow(mean, 2)
         return (variance, pow(variance, 0.5))
     
-    def standardize(self, dataset: NDArray[np.float64]) -> tuple[NDArray[np.float64], np.float64, np.float64]:
-        """
-        Returns (standardized data, mean, standard deviation)
+    def standardize(self, dataset: NDArray[np.float64]) -> NDArray[np.float64]:
+        """Returns the standardization form of a given dataset.
+
+        Parameters
+        ----------
+        dataset : NDArray[np.float64]
+
+        Returns
+        -------
+        NDArray[np.float64]
         """
         mean = self.get_mean(dataset)
         std_dev = self.get_var_std(dataset)[1]
-        return (dataset - mean) / std_dev, mean, std_dev
+        return (dataset - mean) / std_dev
