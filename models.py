bf2c8435e83ee4bd1d97a7337cf731546442017c
Gerum-Berhanu <gerumberhanu0@gmail.com>
2026-03-09 12:25:18 +0300

    applied type hints

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.typing import NDArray
 
 class SimpleLinearRegression:
     """
@@ -7,31 +8,32 @@ class SimpleLinearRegression:
     - do the calculations Ex, Ey, Exy, Ex^2
     - solve for the parameters
     """
-    def __init__(self, input_train_data, output_train_data): 
+    def __init__(self, input_train_data: NDArray[np.float64], output_train_data: NDArray[np.float64]) -> None: 
         """
-        Input Type (of np.array): [[x1,x2,...,xn], [y1,y2,...,yn]]
+        Input Type (of np.array): [x1,x2,...,xn], [y1,y2,...,yn]
         """
-        self.input_train_data = input_train_data
-        self.output_train_data = output_train_data
-        self.n = len(input_train_data)
+        self.input_train_data: NDArray[np.float64] = input_train_data
+        self.output_train_data: NDArray[np.float64] = output_train_data
+        self.n: int = len(input_train_data)
+
         self._calc_()
         self._solve_()
     
-    def _calc_(self):
+    def _calc_(self) -> None:
         self.Ex = np.sum(self.input_train_data)
         self.Ey = np.sum(self.output_train_data)
         self.Exy = np.sum(self.input_train_data * self.output_train_data)
         self.Ex_sqr = np.sum(pow(self.input_train_data, 2))
     
-    def _solve_(self):
+    def _solve_(self) -> None:
         common_denominator = (self.n * self.Ex_sqr) - (self.Ex * self.Ex)
         self.b0 = ( (self.Ey * self.Ex_sqr) - (self.Ex * self.Exy) ) / common_denominator
         self.b1 = ( (self.n * self.Exy) - (self.Ex * self.Ey) ) / common_denominator
 
-    def get_params(self):
+    def get_params(self) -> tuple[np.float64, np.float64]:
         return self.b0, self.b1
 
-    def predict(self, input_test_data):
+    def predict(self, input_test_data: NDArray[np.float64]) -> NDArray[np.float64]:
         """
         Input Type (of np.array): [x1,x2,...,xn]
         Output Type (of np.array): [y1,y2,...,yn]
@@ -39,13 +41,13 @@ class SimpleLinearRegression:
         return self.b1 * input_test_data + self.b0
     
 class ModelKit:
-    def MAE(self, dataset_1, dataset_2):
+    def MAE(self, dataset_1: NDArray[np.float64], dataset_2: NDArray[np.float64]) -> np.float64:
         """
         Returns the mean absolute error between the two given datasets.
         """
         return np.sum(abs(dataset_1 - dataset_2)) / len(dataset_2)
     
-    def split_data(self, dataset, train_percentage):
+    def split_data(self, dataset: NDArray[np.float64], train_percentage: float) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
         """
         Returns (train_data, test_data)
         """
@@ -55,10 +57,10 @@ class ModelKit:
         return train_data, test_data
 
 class StatKit:
-    def get_mean(self, dataset):
+    def get_mean(self, dataset: NDArray[np.float64]) -> np.float64:
         return np.sum(dataset) / len(dataset)
     
-    def get_var_std(self, dataset):
+    def get_var_std(self, dataset: NDArray[np.float64]) -> tuple[np.float64, np.float64]:
         """
         Returns (variance, standard deviation)
         """
@@ -68,7 +70,7 @@ class StatKit:
         variance = (sqr_sum / n) - pow(mean, 2)
         return (variance, pow(variance, 0.5))
     
-    def standardize(self, dataset):
+    def standardize(self, dataset: NDArray[np.float64]) -> tuple[NDArray[np.float64], np.float64, np.float64]:
         """
         Returns (standardized data, mean, standard deviation)
         """
